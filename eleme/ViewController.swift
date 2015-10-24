b9b0bc6acabd18599a660e0eac134a07116778e6
maxiaotiao <addismale@gmail.com>
2015-10-24 09:39:31 +0800

    Uitableview

---
+++ eleme/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  eleme
+//
+//  Created by Addis on 9/11/15.
+//  Copyright (c) 2015 Addis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+       // var label=UILabel(frame:CGRectMake(0,0, 400, 60))
+       // label.text="杭丝联创意产业园"
+        //self.view.addSubview(label);
+      //  label.textColor=UIColor.blackColor()
+       // label.backgroundColor=UIColor.blueColor()
+       // label.textAlignment = .Center
+        
+        
+        
+        
+        
+        
+      //  var button:UIButton = UIButton.buttonWithType(UIButtonType.System) as! UIButton;
+      //  button.frame=CGRectMake(0, 70, 100, 30);
+       // button.setTitle("菜单", forState:UIControlState.Normal)
+       // self.view.addSubview(button);
+        
+        
+        
+       // var button1:UIButton = UIButton.buttonWithType(UIButtonType.System) as! UIButton;
+      //  button1.frame=CGRectMake(0, 70, 400, 30);
+      //  button1.setTitle("照片", forState:UIControlState.Normal)
+      //  self.view.addSubview(button1);
+
+        
+        
+        
+        //var button2:UIButton = UIButton.buttonWithType(UIButtonType.System) as! UIButton;
+       // button2.frame=CGRectMake(0, 70, 650, 30);
+       // button2.setTitle("餐厅", forState:UIControlState.Normal)
+       // self.view.addSubview(button2);
+      
+        
+        
+        
+        
+        
+    }
+}
+        
+        
+        
+        
+        
+  
